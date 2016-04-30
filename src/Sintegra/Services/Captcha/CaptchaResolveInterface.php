6bccb0abd55ae56d0d5e5655610b58c9d291ad7c
Carlos Eduardo <juniob2ss@gmail.com>
2016-04-30 18:15:37 -0300

    Captcha services base class

--- src/Sintegra/Services/Captcha/CaptchaResolveInterface.php
+++ src/Sintegra/Services/Captcha/CaptchaResolveInterface.php
@@ -1,1 +1,35 @@
-CaptchaResolveInterface.php
+<?php namespace zServices\Sintegra\Captcha;
+
+interface CaptchaResolveInterface
+{
+    /**
+     * [$credentials description]
+     * @var array
+     */
+    private $credentials;
+
+    /**
+     * [$service description]
+     * @var [type]
+     */
+    private $service;
+
+    /**
+     * [initialize description]
+     * @return [type] [description]
+     */
+    public function initialize();
+
+    /**
+     * [decode description]
+     * @param  [type] $file    [description]
+     * @param  [type] $timeout [description]
+     * @return string
+     */
+    public function decode($file, $timeout);
+
+    /**
+     * [setCredentials description]
+     */
+    public function setCredentials($credentials);
+}
