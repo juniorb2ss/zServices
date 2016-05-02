4ba60339256a66c7da61d0aade3a25a2ee99957b
Carlos Eduardo <juniob2ss@gmail.com>
2016-05-02 17:23:15 -0300

    Feat: Helpers
    Adicionado Helpers para facilitar o uso da biblioteca.

---
+++ src/Helpers/ReceitaFederal.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!function_exists('receitafederal'))
+{
+	/**
+	 * Helper retorna informações da Receita Federal
+	 * @param  integer $document CNPJ
+	 * @param  string $cookie   cookie request
+	 * @param  string $captcha  Captcha resolvido
+	 * @return array
+	 */
+	function receitaFederal($document = null, $cookie = null, $captcha = null, $params = [])
+	{	
+		/**
+		 * Inicia a classe
+		 * @var object \zServices\ReceitaFederal\Services\Portais\{$portal}\Service
+		 */
+		$search = (new \zServices\ReceitaFederal\Search)->service();
+
+		// Esta pesquisando por um documento?
+		if($document && $cookie && captcha) {
+			$crawler = $search->data($document, $cookie, $captcha, $params);
+
+			return $crawler->scraping();
+		}
+
+		// Se não retorna requisição inicial
+		// com cookie e captcha
+		$search->request();
+
+		return  [
+            'cookie' => $search->cookie(),
+            'image'  => $search->captcha()
+        ];
+	}
+}
---
+++ src/Helpers/Sintegra.php
@@ -0,0 +1,37 @@
+<?php
+if (!function_exists('sintegra'))
+{
+	/**
+	 * Helper retorna informações do sintegra
+	 * @param  integer $document CNPJ or IE
+	 * @param  string $cookie   cookie request
+	 * @param  string $captcha  Captcha resolvido
+	 * @param  array $params   
+	 * @return array
+	 */
+	function sintegra($portal = 'SP', $document = null, $cookie = null, $captcha = null, $params = null )
+	{	
+		/**
+		 * Inicia a classe
+		 * @var object \zServices\Sintegra\Services\Portais\{$portal}\Service
+		 */
+		$search = (new \zServices\Sintegra\Search)->service($portal);
+
+		// Esta pesquisando por um documento?
+		if($document && $cookie && captcha && $params) {
+			$crawler = $search->data($document, $cookie, $captcha, $params);
+
+			return $crawler->scraping();
+		}
+
+		// Se não retorna requisição inicial
+		// com cookie e captcha
+		$search->request();
+
+		return  [
+            'cookie' => $search->cookie(),
+            'image'  => $search->captcha(),
+            'paramBot' => $search->params()['parambot']
+        ];
+	}
+}
