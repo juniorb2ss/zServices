f6e48c1a8ee33f03f1dd7047dab54a51d6f81325
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-05-02 17:18:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Miscellany/Curl.php
+++ src/Miscellany/Curl.php
@@ -7,105 +7,105 @@ namespace zServices\Miscellany;
 class Curl
 {
 
-    /**
-     * [$url description]
-     * @var [type]
-     */
-    private $url;
+	/**
+	 * [$url description]
+	 * @var [type]
+	 */
+	private $url;
 
-    /**
-     * [$options description]
-     * @var [type]
-     */
-    private $options;
+	/**
+	 * [$options description]
+	 * @var [type]
+	 */
+	private $options;
 
-    /**
-     * [$instance description]
-     * @var [type]
-     */
-    private $instance;
+	/**
+	 * [$instance description]
+	 * @var [type]
+	 */
+	private $instance;
 
-    /**
-     * [$response description]
-     * @var [type]
-     */
-    private $response;
+	/**
+	 * [$response description]
+	 * @var [type]
+	 */
+	private $response;
 
-    /**
-     * [init description]
-     * @return [type] [description]
-     */
-    public function init($url)
-    {
-        $this->instance = curl_init($url);
+	/**
+	 * [init description]
+	 * @return [type] [description]
+	 */
+	public function init($url)
+	{
+		$this->instance = curl_init($url);
 
-        $this->url = $url;
+		$this->url = $url;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * [options description]
-     * @param  array  $options [description]
-     * @return [type]          [description]
-     */
-    public function options(array $options)
-    {
-        $this->options = $options;
+	/**
+	 * [options description]
+	 * @param  array  $options [description]
+	 * @return [type]          [description]
+	 */
+	public function options(array $options)
+	{
+		$this->options = $options;
 
-        curl_setopt_array($this->instance, $this->options);
+		curl_setopt_array($this->instance, $this->options);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * [post description]
-     * @return [type]         [description]
-     */
-    public function post(array $fields)
-    {
-        $this->option(CURLOPT_POST, count($fields));
-        $this->option(CURLOPT_POSTFIELDS, http_build_query($fields));
+	/**
+	 * [post description]
+	 * @return [type]         [description]
+	 */
+	public function post(array $fields)
+	{
+		$this->option(CURLOPT_POST, count($fields));
+		$this->option(CURLOPT_POSTFIELDS, http_build_query($fields));
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Set option in cURL
-     * @param  integer $option 
-     * @param  mix $value 
-     */
-    public function option($option, $value)
-    {
-        curl_setopt($this->instance, $option, $value);
-    }
+	/**
+	 * Set option in cURL
+	 * @param  integer $option 
+	 * @param  mix $value 
+	 */
+	public function option($option, $value)
+	{
+		curl_setopt($this->instance, $option, $value);
+	}
 
-    /**
-     * [exec description]
-     * @return [type] [description]
-     */
-    public function exec()
-    {
-        $this->response = curl_exec($this->instance);
-    }
+	/**
+	 * [exec description]
+	 * @return [type] [description]
+	 */
+	public function exec()
+	{
+		$this->response = curl_exec($this->instance);
+	}
 
-    /**
-     * [close description]
-     * @return [type] [description]
-     */
-    public function close()
-    {
-        curl_close($this->instance);
+	/**
+	 * [close description]
+	 * @return [type] [description]
+	 */
+	public function close()
+	{
+		curl_close($this->instance);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * [response description]
-     * @return [type] [description]
-     */
-    public function response()
-    {
-        return $this->response;
-    }
+	/**
+	 * [response description]
+	 * @return [type] [description]
+	 */
+	public function response()
+	{
+		return $this->response;
+	}
 }
--- src/Sintegra/Services/Portais/SP/Crawler.php
+++ src/Sintegra/Services/Portais/SP/Crawler.php
@@ -10,89 +10,89 @@ use zServices\Miscellany\Exceptions\ErrorFoundData;
 class Crawler extends BaseCrawler implements CrawlerInterface
 {
 
-    /**
-     * [$selectors description]
-     * @var [type]
-     */
-    private $selectors = [];
+	/**
+	 * [$selectors description]
+	 * @var [type]
+	 */
+	private $selectors = [];
 
-    /**
-     * [__construct description]
-     * @param [type] $html      [description]
-     * @param array  $selectors [description]
-     */
-    public function __construct($html, $selectors)
-    {
-        $this->selectors = $selectors;
+	/**
+	 * [__construct description]
+	 * @param [type] $html      [description]
+	 * @param array  $selectors [description]
+	 */
+	public function __construct($html, $selectors)
+	{
+		$this->selectors = $selectors;
 
-        parent::__construct($html);
-    }
+		parent::__construct($html);
+	}
 
-    /**
-     * Verifica antes de fazer o crawler se possui erros
-     * na requisição
-     * @return boolean|null 
-     */
-    public function hasError()
-    {
-        $node = $this->scrap($this->selectors['razao_social']);
+	/**
+	 * Verifica antes de fazer o crawler se possui erros
+	 * na requisição
+	 * @return boolean|null 
+	 */
+	public function hasError()
+	{
+		$node = $this->scrap($this->selectors['razao_social']);
 
-        if (!$node->count())
-        {
-            throw new ErrorFoundData($this->clearString($this->scrap($this->selectors['error'])->text()), 1);
-        }
-    }
+		if (!$node->count())
+		{
+			throw new ErrorFoundData($this->clearString($this->scrap($this->selectors['error'])->text()), 1);
+		}
+	}
 
-    /**
-     * Extrai informações do HTML através do DOM
-     *
-     * @return array
-     */
-    public function scraping()
-    {
-        $scrapped = [];
+	/**
+	 * Extrai informações do HTML através do DOM
+	 *
+	 * @return array
+	 */
+	public function scraping()
+	{
+		$scrapped = [];
 
-        $this->hasError();
+		$this->hasError();
 
-        foreach ($this->selectors as $name => $selector) {
-            if (is_string($selector)) {
-                $node = $this->scrap($selector);
+		foreach ($this->selectors as $name => $selector) {
+			if (is_string($selector)) {
+				$node = $this->scrap($selector);
 
-                if ($node->count()) {
-                    $scrapped[$name] = $this->clearString($node->text());
-                }
-            }elseif (is_array($selector)) {
-                foreach ($selector as $selector => $repeat) {
-                    $node = $this->scrap($selector);
-                    if ($node->count()) {
-                        foreach ($node->filter($repeat) as $loop)
-                        {
-                            $scrapped[$name][] = $this->clearString($loop->nodeValue);
-                        }
-                    }
-                }
-            }
-        }
+				if ($node->count()) {
+					$scrapped[$name] = $this->clearString($node->text());
+				}
+			}elseif (is_array($selector)) {
+				foreach ($selector as $selector => $repeat) {
+					$node = $this->scrap($selector);
+					if ($node->count()) {
+						foreach ($node->filter($repeat) as $loop)
+						{
+							$scrapped[$name][] = $this->clearString($loop->nodeValue);
+						}
+					}
+				}
+			}
+		}
 
-        return $scrapped;
-    }
+		return $scrapped;
+	}
 
-    /**
-     * Limpa o valor repassado
-     * @param  string $string
-     * @return string
-     */
-    public function clearString($string)
-    {
-        return trim(preg_replace(['/[\s]+/mu'], ' ', $string));
-    }
+	/**
+	 * Limpa o valor repassado
+	 * @param  string $string
+	 * @return string
+	 */
+	public function clearString($string)
+	{
+		return trim(preg_replace(['/[\s]+/mu'], ' ', $string));
+	}
 
-    /**
-     * Filtra selector no crawler
-     */
-    public function scrap($selector)
-    {
-        $node = $this->filter($selector);
-        return $node;
-    }
+	/**
+	 * Filtra selector no crawler
+	 */
+	public function scrap($selector)
+	{
+		$node = $this->filter($selector);
+		return $node;
+	}
 }
